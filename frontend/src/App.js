058e5caf5ec17e8ff57e68b0725d7638d6300132
DoubleHelix <tjrnyc5@gmail.com>
2021-04-12 18:23:59 -0400

    created UI container with dropdown BTNs template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,107 @@
 import './App.css';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputLabel from "@material-ui/core/InputLabel";
 
 function App() {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary
+    },
+    grid: {
+      margin: 5,
+      // border: '1px solid red',
+      width: "auto!important"
+
+    },
+
+    formControl: {
+      
+      width: "100%"
+      // padding
+    }
+  }));
+
+  const classes = useStyles();
+
+  const [state, setState] = React.useState({
+    age: "",
+    name: "hai"
+  });
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    setState({
+      ...state,
+      [name]: event.target.value
+    });
+  };
+
   return (
     <div>
-      <h1>  Hello Universe !~~! </h1>
+      <Grid container spacing={3} className={classes.grid}>
+        <Grid item xs={6}>
+          <FormControl variant="outlined" className={classes.formControl}>
+            <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
+            <Select
+              native
+              value={state.age}
+              onChange={handleChange}
+              label="Age"
+              inputProps={{
+                name: "age",
+                id: "outlined-age-native-simple"
+              }}
+            >
+              <option aria-label="None" value="" />
+              <option value={10}>Ten</option>
+              <option value={20}>Twenty</option>
+              <option value={30}>Thirty</option>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs={6}>
+          <FormControl variant="outlined" className={classes.formControl}>
+              <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
+              <Select
+                native
+                value={state.age}
+                onChange={handleChange}
+                label="Age"
+                inputProps={{
+                  name: "age",
+                  id: "outlined-age-native-simple"
+                }}
+              >
+                <option aria-label="None" value="" />
+                <option value={10}>Ten</option>
+                <option value={20}>Twenty</option>
+                <option value={30}>Thirty</option>
+              </Select>
+            </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=6</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>xs=3</Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
+
 
 export default App;
