60b37ddff8ce240177b7f1f2191d032376ff9028
DoubleHelix <tjrnyc5@gmail.com>
2021-04-12 22:02:57 -0400

    imported & implemented card material UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,42 @@
 import './App.css';
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import InputLabel from "@material-ui/core/InputLabel";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
 
 function App() {
+  const api = 'https://jsonplaceholder.typicode.com';
+  const [state, setState] = useState({
+    author: "",
+    count: "",
+    name: "",
+    all_posts: ""
+  });
+
+  useEffect(() => {
+    const getAllPosts = () =>
+    fetch(`${api}/posts`)
+    .then(response => response.json())
+    getAllPosts()
+    .then((json) => {
+      console.log("json");
+      setState(prevState => {
+        return {...prevState, all_posts: json };
+      });
+    });   
+
+  }, []);
+  console.log('after state inserted', state.all_posts);
+
   const useStyles = makeStyles((theme) => ({
     root: {
       flexGrow: 1
@@ -17,84 +46,120 @@ function App() {
       textAlign: "center",
       color: theme.palette.text.secondary
     },
-    grid: {
+    grid_container: {
       margin: 5,
       // border: '1px solid red',
       width: "auto!important"
-
     },
 
     formControl: {
-      
       width: "100%"
-      // padding
-    }
+    },
+    bullet: {
+      display: 'inline-block',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 14,
+    },
+    pos: {
+      marginBottom: 12,
+    },
   }));
 
-  const classes = useStyles();
+  const classes = useStyles();  
+  const bull = <span className={classes.bullet}>•</span>;
+  
 
-  const [state, setState] = React.useState({
-    age: "",
-    name: "hai"
-  });
-
-  const handleChange = (event) => {
-    const name = event.target.name;
+  const handleAuthorChange = (event) => {
+    const author = event.target.name;
     setState({
       ...state,
-      [name]: event.target.value
+      [author]: event.target.value
     });
+    // setState(prevState => {
+    //   return {...prevState, author: author };
+    // });
   };
 
+  const handleCountChange = (event) => {
+    const count = event.target.name;
+    setState({
+      ...state,
+      [count]: event.target.value
+    });
+    // setState(prevState => {
+    //   return {...prevState, count: count };
+    // });
+  };
+  if (state.all_posts && state.count){
+    console.log('count', parseInt(state.count), 'state', state.all_posts[0].title, 'array', [...Array(parseInt(state.count)-1)])
+  }
   return (
     <div>
-      <Grid container spacing={3} className={classes.grid}>
+      <Grid container spacing={3} className={classes.grid_container}>
         <Grid item xs={6}>
           <FormControl variant="outlined" className={classes.formControl}>
-            <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
+            <InputLabel htmlFor="outlined-age-native-simple">Author</InputLabel>
             <Select
               native
-              value={state.age}
-              onChange={handleChange}
-              label="Age"
+              value={state.author}
+              onChange={handleAuthorChange}
+              label="Author"
               inputProps={{
-                name: "age",
-                id: "outlined-age-native-simple"
+                name: "author",
+                id: "outlined-author-native-simple"
               }}
             >
               <option aria-label="None" value="" />
-              <option value={10}>Ten</option>
-              <option value={20}>Twenty</option>
-              <option value={30}>Thirty</option>
+              <option value={1}>Name 1</option>
+              <option value={2}>Name 2</option>
+              <option value={3}>Name 3</option>
             </Select>
           </FormControl>
         </Grid>
         <Grid item xs={6}>
           <FormControl variant="outlined" className={classes.formControl}>
-              <InputLabel htmlFor="outlined-age-native-simple">Age</InputLabel>
+              <InputLabel htmlFor="outlined-age-native-simple">Count</InputLabel>
               <Select
                 native
-                value={state.age}
-                onChange={handleChange}
-                label="Age"
+                value={state.count}
+                onChange={handleCountChange}
+                label="Count"
                 inputProps={{
-                  name: "age",
-                  id: "outlined-age-native-simple"
+                  name: "count",
+                  id: "outlined-count-native-simple"
                 }}
               >
                 <option aria-label="None" value="" />
-                <option value={10}>Ten</option>
-                <option value={20}>Twenty</option>
-                <option value={30}>Thirty</option>
+                <option value={2}>2</option>
+                <option value={5}>5</option>
+                <option value={10}>10</option>
               </Select>
             </FormControl>
         </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=6</Paper>
-        </Grid>
-        <Grid item xs={12}>
-          <Paper className={classes.paper}>xs=3</Paper>
-        </Grid>
+        {state.all_posts && state.count && ([...Array(parseInt(state.count)-1)].map((e, i) => ( 
+          <Grid item xs={12} key={state.all_posts[i].id}>
+            <Card className={classes.root} variant="outlined">
+              <CardContent>
+                <Typography variant="h5" component="h2">
+                  {state.all_posts[i].title}
+                </Typography>
+                <Typography variant="body2" component="p">
+                {state.all_posts[i].body}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          )))}
+
+
+       
+
         <Grid item xs={12}>
           <Paper className={classes.paper}>xs=3</Paper>
         </Grid>
