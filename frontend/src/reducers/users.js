2c75bebd9be69ad11368b945d75353ad1219d9db
DoubleHelix <tjrnyc5@gmail.com>
2021-04-14 13:46:46 -0400

    created users reducer file

---
+++ frontend/src/reducers/users.js
@@ -0,0 +1,23 @@
+import {
+  RECEIVE_USERS,
+  RECEIVE_USERS_FAIL,
+} from "../actions/users";
+
+export default function users(state = {}, action) {
+
+  switch (action.type) {
+    case RECEIVE_USERS:{
+      return {
+        ...state,
+        ...action.payload,
+      };}
+      //MAKE ERROR REDUCER
+      case RECEIVE_USERS_FAIL:
+        return {
+          ...state,
+          ...action.payload.err,
+        };
+    default:
+      return state;
+  }
+}
