faa260c24caa466a72e3e6ca7a562ed6c0711fd4
DoubleHelix <tjrnyc5@gmail.com>
2021-04-14 13:45:59 -0400

    created shared.js file for handling initial data dispatch

--- frontend/src/actions/shared.js
+++ frontend/src/actions/shared.js
@@ -1,6 +1,9 @@
 import { _getAllUsers} from "../utils/_DATA";
-// import { receiveUsers } from "../actions/users";
+import { receiveUsers } from "../actions/users";
 // import { receiveQuestions } from "../actions/questions";
+import {
+  RECEIVE_USERS_FAIL,
+} from "../actions/users";
 
 import { showLoading, hideLoading } from "react-redux-loading";
 
@@ -11,11 +14,11 @@ export function handleInitialData() {
   return (dispatch) => {
     dispatch(showLoading());
     return _getAllUsers()
-    .then((json) => {
-      setState(prevState => {
-        return {...prevState, users: json };
-      });
-    });   
+    .then((users) => {
+      dispatch(receiveUsers(users));
+      dispatch(hideLoading());
+    })
+    .catch((err) => dispatch ({type: RECEIVE_USERS_FAIL, payload: err}));   
   }
 }
 
@@ -23,3 +26,7 @@ export function handleInitialData() {
 // const _getAllUsers = () =>
 //     fetch(`${api}/users`)
 //     .then(response => response.json());
+
+// setState(prevState => {
+      //   return {...prevState, users: json };
+      // });
