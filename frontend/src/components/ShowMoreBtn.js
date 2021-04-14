0558e48e04d29706a4707d5cace87efb5ba6bd12
DoubleHelix <tjrnyc5@gmail.com>
2021-04-14 00:16:56 -0400

    installed redux, react-redux, and redux-thunk

---
+++ frontend/src/components/ShowMoreBtn.js
@@ -0,0 +1,25 @@
+import Grid from "@material-ui/core/Grid";
+import Button from '@material-ui/core/Button';
+import React, {useState, useEffect} from "react";
+
+export default function ShowMoreBtn() {
+   
+
+    return (
+        <Grid item xs={12}> {/* Author-ShowMoreBtn */}
+            {/* {(state.count && state.author ) && ((state.count + state.additionalViews ) < state.posts.length) ? (
+            <Button variant="outlined" color="primary" onClick= {showMore} className={classes.showMoreBtn}>
+                    SHOW MORE
+            </Button>
+            ):
+            (
+            <Button variant="outlined" color="primary" className={classes.showMoreBtn} disabled>
+                    SHOW MORE
+            </Button>
+            )
+            } */}
+        </Grid>
+    )
+
+
+}
