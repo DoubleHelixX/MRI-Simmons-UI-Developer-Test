675e571d6e84d5d04653376bcada977129e2f30b
DoubleHelix <tjrnyc5@gmail.com>
2021-04-14 22:36:16 -0400

    added more reducer files to combined reducer

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,17 @@
 import { combineReducers } from "redux";
 import users from "./users";
+import count from "./count";
+import author from "./author";
+import posts from "./posts";
+import additionalViews from "./additionalViews";
+
 import { loadingBarReducer } from "react-redux-loading";
 
 export default combineReducers({
   users,
+  count,
+  author,
+  posts,
+  additionalViews,
   loadingBar: loadingBarReducer,
 });
