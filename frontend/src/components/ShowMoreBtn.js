c26f1a525568b5dd935dfe336aedd50fab5d196d
DoubleHelix <tjrnyc5@gmail.com>
2021-04-14 22:43:27 -0400

    set up show more btn component with functionality for both retrieving data from redux store

--- frontend/src/components/ShowMoreBtn.js
+++ frontend/src/components/ShowMoreBtn.js
@@ -1,25 +1,52 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { incrementAdditionalViews } from "../actions/additionalViews";
+
 import Grid from "@material-ui/core/Grid";
-import Button from '@material-ui/core/Button';
-import React, {useState, useEffect} from "react";
+import Button from "@material-ui/core/Button";
+
+import { useStyles } from "../utils/_Constants";
 
 export default function ShowMoreBtn() {
-   
+  const classes = useStyles();
 
-    return (
-        <Grid item xs={12}> {/* Author-ShowMoreBtn */}
-            {/* {(state.count && state.author ) && ((state.count + state.additionalViews ) < state.posts.length) ? (
-            <Button variant="outlined" color="primary" onClick= {showMore} className={classes.showMoreBtn}>
-                    SHOW MORE
-            </Button>
-            ):
-            (
-            <Button variant="outlined" color="primary" className={classes.showMoreBtn} disabled>
-                    SHOW MORE
-            </Button>
-            )
-            } */}
-        </Grid>
-    )
+  const dispatch = useDispatch();
+  const { posts, count, author, additionalViews } = useSelector((state) => ({
+    posts: state.posts,
+    count: state.count,
+    author: state.author,
+    additionalViews: state.additionalViews,
+  }));
 
+  const showMore = (event) => {
+    event.preventDefault();
+    dispatch(incrementAdditionalViews(additionalViews + 2));
+  };
 
+  return (
+    <Grid item xs={12}>
+      {count &&
+      author &&
+      count + additionalViews < Object.keys(posts).length ? (
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={showMore}
+          className={classes.showMoreBtn}
+        >
+          SHOW MORE
+        </Button>
+      ) : (
+        <Button
+          variant="outlined"
+          color="primary"
+          className={classes.showMoreBtn}
+          disabled
+        >
+          SHOW MORE
+        </Button>
+      )}
+    </Grid>
+  );
 }
