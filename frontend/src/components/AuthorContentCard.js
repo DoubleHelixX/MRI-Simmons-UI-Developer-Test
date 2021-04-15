4ed8f7a0363f9a0fefd4fffdf93cdcdd2c6f492c
DoubleHelix <tjrnyc5@gmail.com>
2021-04-14 22:44:19 -0400

    set up 'author content' component with functionality for retrieving data from redux store

--- frontend/src/components/AuthorContentCard.js
+++ frontend/src/components/AuthorContentCard.js
@@ -1,47 +1,57 @@
-import React, {useState, useEffect} from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { useSelector } from "react-redux";
 
+import { useStyles } from "../utils/_Constants";
+
+import Grid from "@material-ui/core/Grid";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Badge from "@material-ui/core/Badge";
+import CommentIcon from "@material-ui/icons/Comment";
 
 export default function AuthorContentCard() {
-    const useStyles = makeStyles((theme) => ({
-        root: {
-          flexGrow: 1
-        }
-      }));
-    
-      const classes = useStyles();
+  const { posts, count, author, additionalViews } = useSelector((state) => ({
+    posts: state.posts,
+    count: state.count,
+    author: state.author,
+    additionalViews: state.additionalViews,
+  }));
+  const classes = useStyles();
 
-    return (
-        <Grid item xs={12} 
-        // key={state.posts[i].id}
-        > 
+  return (
+    <React.Fragment>
+      {count > 0 &&
+        author &&
+        Object.keys(posts).length > 0 &&
+        [
+          ...Array(
+            count + additionalViews > Object.keys(posts).length
+              ? Object.keys(posts).length
+              : count + additionalViews
+          ),
+        ].map((e, i) => (
+          <Grid item xs={12} key={posts[i].id}>
             <Card className={classes.root} variant="outlined">
-            <CardContent>
+              <CardContent>
                 <Typography variant="h5" component="h2">
-                {/* {state.posts[i].title} */}
+                  {posts[i].title}
                 </Typography>
                 <Typography variant="body1" component="p">
-                {/* {state.posts[i].body} */}
+                  {posts[i].body}
                 </Typography>
-            </CardContent>
-            <CardActions> 
-                {/* {state.posts[i].totalComments &&(  
-                <Badge badgeContent={state.posts[i].totalComments} color="primary">
-                <CommentIcon/>
-                </Badge>
-                )} */}
-            </CardActions>
+              </CardContent>
+              <CardActions>
+                {posts[i].totalComments && (
+                  <Badge badgeContent={posts[i].totalComments} color="primary">
+                    <CommentIcon />
+                  </Badge>
+                )}
+              </CardActions>
             </Card>
-        </Grid>
-    )
-
-
+          </Grid>
+        ))}
+    </React.Fragment>
+  );
 }
-
-
-
